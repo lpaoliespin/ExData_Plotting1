107f32802629534ac7ff08416c45b17df8fa62ea
Lucrecia Paolini <lpaoliespin@gmail.com>
2023-04-24 00:09:32 -0400

    Add missing comments to plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,12 @@
 # Plot 1. Histogram of Global active power consumption
 
-# Create the histogram
+# The hist() function creates a histogram plot of the data. The first argument, 
+# hpc$Global_active_power, specifies the data to be plotted. The second argument,
+# main="Global Active Power", sets the title of the plot to "Global Active Power".
+# The third argument, xlab="Global Active Power (kilowatts)", sets the label for
+# the x-axis to "Global Active Power (kilowatts)". The fourth argument, ylab="Frequency",
+# sets the label for the y-axis to "Frequency". Finally, the fifth argument, col="red",
+# sets the color of the bars in the histogram to red.
 hist(hpc$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", 
      ylab="Frequency", col="red")
 
